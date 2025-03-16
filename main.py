5b8168bd9e4a6b35a77849dc29fca02dcd4fbc00
NavajitD <navjit.zephyr@gmail.com>
2025-03-16 12:20:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ def get_category_prediction(expense_name):
 # Function to submit data to Google Apps Script
 def submit_to_google_apps_script(data):
     # Replace with your deployed Google Apps Script web app URL
-    apps_script_url = "https://script.google.com/macros/s/AKfycbyTqMtjJC1QGLc8ZMY0gti8iTqejrCiWS8Ezip3h_nV6VXifv1vP8Uz-PC34r2-8pHx2Q/exec"
+    apps_script_url = "https://script.google.com/macros/s/AKfycbyyvQylzEz7uxrpizadLThdrDl5z3fGG2kV7InhLissWKH2uw1uzPCfi6TzDLi_iRB8Hg/exec"
     
     try:
         headers = {"Content-Type": "application/json"}
